40e287ff648ed56478ce3441462d44b9dc8c24bd
silvina-riera <silvinariera83@gmail.com>
2024-05-15 23:24:24 -0300

    fix Arrivals / Departures classes

--- ApiAirportManager/src/main/java/com/ar/bootcampJava/ApiAirportManager/models/Departures.java
+++ ApiAirportManager/src/main/java/com/ar/bootcampJava/ApiAirportManager/models/Departures.java
@@ -2,8 +2,6 @@ package com.ar.bootcampJava.ApiAirportManager.models;
 
 import com.ar.bootcampJava.ApiAirportManager.models.enums.Status;
 import jakarta.persistence.*;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -19,8 +17,6 @@ public class Departures {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @NotNull(message = "Debe ingresar un vuelo.")
-    @NotBlank(message = "Debe ingresar un vuelo.")
     private Long flight_id;
 
     private String destination;
@@ -29,8 +25,6 @@ public class Departures {
 
     private String company;
 
-    @NotNull(message = "Debe ingresar el estado del vuelo.")
-    @NotBlank(message = "Debe ingresar el estado del vuelo.")
     private Status status;
 
     private String gate;
